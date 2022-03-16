14b501b2be2a6c0e6f1d1742ee825030de24f82b
Farah Salsabila <farahsalsabila855@gmail.com>
2022-03-16 14:38:28 +0700

    Praktikum

---
+++ Praktikum2-route/Praktikum-laravel2/resources/views/routes/mahasiswa.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+</body>Daftar Mahasiswa</body>
+<ol>
+    <li>?php echo $Mahasiswa01; ?</li>
+    <li>Khairul khazil</li>
+    <li>Anisa Salwa</li>
+    <li>Sepkha Wulan Dari</li>
+</ol>
+</body>
+</html>
